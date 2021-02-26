0b61665d46ac0aa77f9b85feab00ef4128ae0cf3
caoting <307298900@qq.com>
2021-02-26 13:02:31 +0800

    feat: spell mysql dsn

--- module3/05common/mysql.go
+++ module3/05common/mysql.go
@@ -1,6 +1,10 @@
 package common
 
-import "github.com/micro/go-micro/v2/config"
+import (
+	"strconv"
+
+	"github.com/micro/go-micro/v2/config"
+)
 
 type MysqlConf struct {
 	Host     string `json:"host"`
@@ -12,6 +16,10 @@ type MysqlConf struct {
 
 func GetMysqlFromConsul(config config.Config, path ...string) *MysqlConf {
 	conf := &MysqlConf{}
-	config.Get(path...).Scan(conf)
+	_ = config.Get(path...).Scan(conf)
 	return conf
+}
+
+func GetMysqlDSN(mysqlInfo MysqlConf) string {
+	return mysqlInfo.User + ":" + mysqlInfo.Pwd + "@tcp(" + mysqlInfo.Host + ":" + strconv.FormatInt(mysqlInfo.Port, 10) + ")/" + mysqlInfo.Database + "?charset=utf8&parseTime=True&loc=Local"
 }
